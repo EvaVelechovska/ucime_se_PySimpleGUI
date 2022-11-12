d64a1fafccfa4766413649ced76834c136ed353d
eva <hanzlikova.eva@seznam.cz>
2022-11-12 17:47:58 +0100

    Záložka pokus
    po zmáčknutí další záznam se vymažou zdané hodnoty z okýnka

--- pokus.py
+++ pokus.py
@@ -3,13 +3,14 @@ import PySimpleGUI as sg
 sg.theme("DarkBlue")
 
 layout = ([sg.Text("Ahoj světe")],
-          [sg.Text("Zadej text"), sg.InputText(key="Input1")],
-          [sg.Text("Zadej další text"), sg.InputText(key="Input2")],
-          [sg.Push(), sg.Button("OK"), sg.Button("Cancel")])
+          [sg.Text("Zadej text"), sg.InputText(key="Input1", do_not_clear=True)],
+          [sg.Text("Zadej další text"), sg.InputText(key="Input2", do_not_clear=True)],
+          [sg.Button("Další záznam"), sg.Push(), sg.Button("OK"), sg.Button("Cancel")])
 
 window = sg.Window("Pozdrav", layout, finalize=True)
 window["Input1"].bind("<Return>", "_Enter")
 window["Input2"].bind("<Return>", "_Enter")
+
 
 while True:
     event, values = window.read()
@@ -19,6 +20,10 @@ while True:
         print(event)
     elif event == "Input2" + "_Enter":
         print(event)
+    elif event == "Další záznam":
+        print(values)
+        window["Input1"].Update("")
+        window["Input2"].Update("")
     elif event == "OK":
         print(values)
 
